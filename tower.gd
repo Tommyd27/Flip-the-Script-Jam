d692f5f98673b1ebd10b06a7cfd3bb9cf0acab06
Stewbbb <stewart.smyth3@gmail.com>
2023-07-20 13:46:14 +0100

    Add Arrow

--- tower.gd
+++ tower.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Bow").play("Shoot")
+	for i in range(10):
+		get_node("Bow").play("Shoot")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
