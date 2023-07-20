4cf1d7fe9b1b03a83689e49204266ed67c68e89f
Stewbbb <stewart.smyth3@gmail.com>
2023-07-20 13:59:31 +0100

    ArrowAndTower

--- tower.gd
+++ tower.gd
@@ -1,12 +1,27 @@
 extends Node2D
 
 
+var arrow = preload("res://arrow.tscn")
+
+var time = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(10):
-		get_node("Bow").play("Shoot")
+	pass
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func shoot(direction):
+	get_node("Bow").play("Shoot")
+	var arrowTemp = arrow.instantiate()
+	add_child(arrowTemp)
+	
+
+func _on_timer_timeout():
+	shoot(180)
+	
